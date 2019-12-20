04cef9494494090277db9fe64cfc4f3c082835db
bhers4 <bhers4@gmail.com>
2019-12-19 18:53:17 -0800

    Changed Tutorial Name and add CNN

---
+++ machine_learning/PyTorchTutorials/basic_classifier/classifier.py
@@ -0,0 +1,146 @@
+'''
+    Title: Training a Classifier
+    Link: https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html#sphx-glr-beginner-blitz-cifar10-tutorial-py
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Convolutional Neural Network
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    
+    def forward(self, x):
+        '''
+            Structure:
+                Convolution Layer 1
+                Relu Layer
+                Max Pooling
+                Convolution Layer 2
+                Relu Layer
+                Max Pooling
+                Flattening Layer
+                Fully Connected Layer 1
+                Relu Layer
+                Fully Connected Layer 2
+                Relu Layer
+                Fully Connected Layer 3
+        '''
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16*5*5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+def imshow(img):
+    img = img/2+0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+# Training Data
+trainset = torchvision.datasets.CIFAR10(root="./data", train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,shuffle=True,num_workers=0)
+# Validation Data
+testset = torchvision.datasets.CIFAR10(root="./data", train=False, download=True, transform = transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=0)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Get Random Training Images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+print(''.join('%5s' % classes[labels[j]] for j in range(4)))
+
+# Make the CNN object
+net = CNN()
+# Select loss function
+criterion = nn.CrossEntropyLoss()
+# Select optimizer function
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+# Train Network
+for epoch in range(2):
+    print("Epoch: ", epoch)
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # Get inputs
+        inputs, labels = data
+        # Zero optimizer
+        optimizer.zero_grad()
+        # Training step
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        if i % 2000 == 1999:
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i+1, running_loss/2000))
+            running_loss = 0.0
+print("Finished Training")
+# Save Model 
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+# Test Network
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+# Print images
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+# Just showing how to do loading
+net = CNN()
+net.load_state_dict(torch.load(PATH))
+
+outputs = net(images)
+_, predicted = torch.max(outputs, 1)
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
---
+++ machine_learning/readme.md
@@ -0,0 +1,3 @@
+# Projects dealing with Machine Learning and Artifical Intelligence
+* PyTorch Tutorials
+    * Contains projects from PyTorch as an overview on PyTorch framework
