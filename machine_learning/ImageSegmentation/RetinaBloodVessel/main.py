051fc33841e6a13630a5114e86d3ab1daf5f4496
Ben <bhers4@gmail.com>
2020-02-09 16:01:47 -0800

    Fixed image size issue as well as first draft of training loop

--- machine_learning/ImageSegmentation/RetinaBloodVessel/main.py
+++ machine_learning/ImageSegmentation/RetinaBloodVessel/main.py
@@ -10,6 +10,7 @@ import glob
 import os, sys
 import imageio
 import random
+from PIL import Image
 
 class UNet(nn.Module):
     def __init__(self, in_channels=3, out_channels=1, init_features=32):
@@ -42,7 +43,9 @@ class UNet(nn.Module):
         enc3 = self.encoder3(self.pool2(enc2))
         enc4 = self.encoder4(self.pool3(enc3))
         bottleneck = self.bottleneck(self.pool4(enc4))
+        # print("Bottleneck: ", bottleneck.shape)
         dec4 = self.upconv4(bottleneck)
+        # print("DEC4: ", dec4.shape, " ENC4: ", enc4.shape)
         dec4 = torch.cat((dec4, enc4), dim=1) # This combines encoder4 and decoder 4
         dec4 = self.decoder4(dec4)
         dec3 = self.upconv3(dec4)
@@ -54,7 +57,7 @@ class UNet(nn.Module):
         dec1 = self.upconv1(dec2)
         dec1 = torch.cat((dec1, enc1), dim=1)
         dec1 = self.decoder1(dec1)
-        return torch.sigmoid(self.conv(dec1))
+        return torch.sigmoid(self.conv(dec1))*255
     @staticmethod
     def _block(in_channels, features, name):
         return nn.Sequential( OrderedDict( [
@@ -79,48 +82,54 @@ class UNet(nn.Module):
 class RetinaDataset(Dataset):
     def __init__(self, imageData, imageTarget, transform=None,randomSampling=True):
         self.to_tensor = transforms.ToTensor()
-        self.images = {}
-        self.masks = {}
+        self.images = []
+        self.masks = []
+        # Make this not fixed later
+        self.randCrop = transforms.RandomCrop((512, 512))
+        self.resize = transforms.Resize((512, 512))
         # Load original images
         for item in glob.glob(os.path.join(imageData, "*.png")):
             img = imageio.imread(item)
             name = item.split("\\")[1]
-            self.images[name] = img
+            self.images.append(img)
         # Load segmented images
         for item in glob.glob(os.path.join(imageTarget, "*.png")):
             img = imageio.imread(item)
             name = item.split("\\")[1]
-            self.masks[name] = img
+            self.masks.append(img)
         assert(len(self.images)==len(self.masks), "Don't have the same amount of images and masks")
     
     def __len__(self):
         return len(self.images)
     
     def __getitem__(self, index):
-        print("INDEX: ", index)
-        return self.images[index]
+        img = Image.fromarray(np.asarray(self.images[index]))
+        mask = Image.fromarray(np.asarray(self.masks[index]))
+        img = torch.from_numpy(np.array(self.resize(img), dtype=np.float32).transpose(2, 0, 1))
+        mask = torch.from_numpy(np.array(self.resize(mask), dtype=np.float32))
+        return img, mask
     
 def trainTestSplit(imageData, imageTarget, trainImage, trainTarget, testImage, testTarget):
-    images = {}
-    masks = {}
+    images = []
+    masks = []
     # Load original images
     for item in glob.glob(os.path.join(imageData, "*.png")):
         img = imageio.imread(item)
         name = item.split("\\")[1]
-        images[name] = img
+        images.append(img)
     # Load segmented images
     for item in glob.glob(os.path.join(imageTarget, "*.png")):
         img = imageio.imread(item)
         name = item.split("\\")[1]
-        masks[name] = img
-    trainImages = random.sample(list(images), int(len(images)*0.75))
+        masks.append(img)
+    trainImages = random.sample(images, int(len(images)*0.75))
     for item in trainImages:
         img = imageio.imread(imageData+item)
         imgTarget = imageio.imread(imageTarget+item)
         imageio.imwrite(trainImage+item, img)
         imageio.imwrite(trainTarget+item, imgTarget)
     # Find the rest
-    for item in images.keys():
+    for item in images:
         if item not in trainImages:
             print("Test: ", item)
             img = imageio.imread(imageData+item)
@@ -155,8 +164,32 @@ if __name__ == "__main__":
     optimizer = optim.Adam(model.parameters(), lr=learningRate)
     epochs = 100
     loaders = {"train": loaderTrain, "valid": loaderTest}
+    losses = []
+    epochLosses = []
     for epoch in range(epochs):
         model.train()
+        eLosses = []
         for i, data in enumerate(loaders['train']):
-            x, y = data
-            print("X: ", x, " Y: ", y)
+            img, mask = data
+            optimizer.zero_grad()
+            print("Img shape: ", img.shape, " Mask shape: ", mask.shape)
+
+            with torch.set_grad_enabled(True):
+                maskPred = model(img)
+                maskLoss = loss(maskPred, mask)
+                losses.append(maskLoss)
+                maskLoss.backward()
+                optimizer.step()
+                eLosses.append(maskLoss)
+        epochLosses.append(eLosses)
+        print("Epoch: %d, Loss: %f" %(epoch, np.mean(eLosses)))
+    torch.save(model, 'unet.pt')
+
+    for i, data in enumerate(testData):
+        img, mask = data
+        maskPred = model(img)
+        maskLoss = loss(maskPred, mask)
+        print("Loss: ", maskLoss)
+        print(type(maskPred))
+        imageio.imshow(maskPred)
+            
