976d10fcae4184bb75d7a22bd1a2d0b87d416e83
Ben Hers <bhers4@gmail.com>
2020-04-21 19:31:07 -0700

    Did some more flask

--- flask/basic/index.py
+++ flask/basic/index.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 import json
 import time
+from PIL import Image
 
 app = Flask(__name__)
 
@@ -15,6 +16,22 @@ def testButton():
         time.sleep(1)
         print(i)
     return json.dumps({'status': 'OK'})
+
+@app.route('/files', methods=['POST'])
+def testFiles():
+    print("Testfiles")
+    if request.method == 'POST':
+        print("In Post")
+        for item in request.files.items():
+            print("Item: ", item)
+            print(type(item[1]))
+            try:
+                pilImg = Image.open(item[1])
+                pilImg.show()
+            except:
+                print("PIL Image open failed")
+        return json.dumps({'status':'OK'})
+    return json.dumps({'status':'OK'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
